84c9e8315a76c77737f15c9496944077e348b17e
AntoniaGeorgieva <146659899+AntoniaGeorgieva@users.noreply.github.com>
2024-07-17 15:55:10 +0300

    add solution for 07. Cooking by Numbers

--- 01.Syntax, Functions and Statements - Exercise/06.roadRadar.js
+++ 01.Syntax, Functions and Statements - Exercise/06.roadRadar.js
@@ -1,70 +1,70 @@
 function roadRadar(speed, area) {
   switch (area) {
-    case "motorway": 
-    if(speed <= 130) {
-      console.log(`Driving ${speed} km/h in a 130 zone`);
-    } else {
-      let difference = Number(speed) - 130;
-      let status;
-      if(difference <= 20) {
-        status = "speeding";
-      } else if (difference <= 40) {
-        status = "excessive speeding";
+    case "motorway":
+      if (speed <= 130) {
+        console.log(`Driving ${speed} km/h in a 130 zone`);
       } else {
-        status = "reckless driving";
+        let difference = Number(speed) - 130;
+        let status;
+        if (difference <= 20) {
+          status = "speeding";
+        } else if (difference <= 40) {
+          status = "excessive speeding";
+        } else {
+          status = "reckless driving";
+        }
+        console.log(`The speed is ${difference} km/h faster than the allowed speed of 130 - ${status}`);
       }
-      console.log(`The speed is ${difference} km/h faster than the allowed speed of 130 - ${status}`);  
-    }
-    break;
-    case "interstate": 
-    if(speed <= 90) {
-      console.log(`Driving ${speed} km/h in a 90 zone`);
-    } else {
-      let difference = Number(speed) - 90;
-      let status;
-      if(difference <= 20) {
-        status = "speeding";
-      } else if (difference <= 40) {
-        status = "excessive speeding";
+      break;
+    case "interstate":
+      if (speed <= 90) {
+        console.log(`Driving ${speed} km/h in a 90 zone`);
       } else {
-        status = "reckless driving";
+        let difference = Number(speed) - 90;
+        let status;
+        if (difference <= 20) {
+          status = "speeding";
+        } else if (difference <= 40) {
+          status = "excessive speeding";
+        } else {
+          status = "reckless driving";
+        }
+        console.log(`The speed is ${difference} km/h faster than the allowed speed of 90 - ${status}`);
       }
-      console.log(`The speed is ${difference} km/h faster than the allowed speed of 90 - ${status}`);
-    }
-    break;
-    case "city": 
-    if(speed <= 50) {
-      console.log(`Driving ${speed} km/h in a 50 zone`);
-    } else {
-      let difference = Number(speed) - 50;
-      let status;
-      if(difference <= 20) {
-        status = "speeding";
-      } else if (difference <= 40) {
-        status = "excessive speeding";
+      break;
+    case "city":
+      if (speed <= 50) {
+        console.log(`Driving ${speed} km/h in a 50 zone`);
       } else {
-        status = "reckless driving";
+        let difference = Number(speed) - 50;
+        let status;
+        if (difference <= 20) {
+          status = "speeding";
+        } else if (difference <= 40) {
+          status = "excessive speeding";
+        } else {
+          status = "reckless driving";
+        }
+        console.log(`The speed is ${difference} km/h faster than the allowed speed of 50 - ${status}`);
       }
-      console.log(`The speed is ${difference} km/h faster than the allowed speed of 50 - ${status}`);
-    }
-    break;
-    case "residential": 
-    if(speed <= 20) {
-      console.log(`Driving ${speed} km/h in a 20 zone`);
-    } else {
-      let difference = Number(speed) - 20;
-      let status;
-      if(difference <= 20) {
-        status = "speeding";
-      } else if (difference <= 40) {
-        status = "excessive speeding";
+      break;
+    case "residential":
+      if (speed <= 20) {
+        console.log(`Driving ${speed} km/h in a 20 zone`);
       } else {
-        status = "reckless driving";
+        let difference = Number(speed) - 20;
+        let status;
+        if (difference <= 20) {
+          status = "speeding";
+        } else if (difference <= 40) {
+          status = "excessive speeding";
+        } else {
+          status = "reckless driving";
+        }
+        console.log(`The speed is ${difference} km/h faster than the allowed speed of 20 - ${status}`);
       }
-      console.log(`The speed is ${difference} km/h faster than the allowed speed of 20 - ${status}`);
-    }
-    break;
+      break;
   }
 }
 
-roadRadar (21, 'residential')
+roadRadar(21, 'residential')
---
+++ 01.Syntax, Functions and Statements - Exercise/07.cookingByNumbers.js
@@ -0,0 +1,30 @@
+function cookingByNumbers(startingPoint, operation1, operation2, operation3, operation4, operation5) {
+  let finalPoint = Number(startingPoint);
+
+  let operations = operation1 + " " + operation2 + " " + operation3 + " " + operation4 + " " + operation5;
+  let operationsArr = operations.toString().split(" ");
+  
+  for (const operation of operationsArr) {
+    switch (operation) {
+      case "chop":
+        finalPoint /= 2;
+        break;
+      case "dice":
+        finalPoint = Math.sqrt(finalPoint);
+        break;
+      case "spice":
+        finalPoint += 1;
+        break;
+      case "bake":
+        finalPoint *= 3;
+        break;
+      case "fillet":
+        finalPoint -= (0.2 * finalPoint);
+        break;
+    }
+    console.log(finalPoint);
+  }
+  
+}
+
+cookingByNumbers('9', 'dice', 'spice', 'chop', 'bake', 'fillet')
