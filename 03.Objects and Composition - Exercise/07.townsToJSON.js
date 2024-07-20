b6f6a731f2f3e8828896409a572817130df6bf6f
AntoniaGeorgieva <146659899+AntoniaGeorgieva@users.noreply.github.com>
2024-07-20 17:45:08 +0300

    Update 07.townsToJSON.js

--- 03.Objects and Composition - Exercise/07.townsToJSON.js
+++ 03.Objects and Composition - Exercise/07.townsToJSON.js
@@ -3,17 +3,12 @@ function townsToJSON(arr) {
 
   for (let i = 1; i < arr.length; i++) {
     let line = arr[i].split(' | ');
-    let Town = line[0]
-    let Latitude = Number(line[1]).toFixed(1);
-    let Longitude = Number(line[2]).toFixed(1);
+    let Town = line[0].split('| ')[1];
+    let Latitude = Number(Number(line[1]).toFixed(2));
+    let Longitude = Number(Number(line[2].split(' |')[0]).toFixed(2));
 
     result.push({ Town, Latitude, Longitude });
   }
 
   console.log(JSON.stringify(result));
 }
-
-townsToJSON(['| Town | Latitude | Longitude |',
-  '| Sofia | 42.696552 | 23.32601 |',
-  '| Beijing | 39.913818 | 116.363625 |']
-  )
